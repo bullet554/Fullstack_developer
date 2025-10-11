accf366faba6b1b3d8d31f02d553c28d75ba8aed
Ilya Karpov <ilya.karpov.93@mail.ru>
2025-10-11 23:43:36 +0400

    completed seminar 2

---
+++ 2._JavaScript/Practical/Seminars/Seminar_002/Task_004/script.js
@@ -0,0 +1,38 @@
+/* Задание 4 (тайминг 15 минут)
+1. Не запуская код, определите, что выведется в консоль:
+a. console.log(String(true));
+2. Не запуская код, определите, что выведется в консоль:
+a. console.log('a' + true);
+3. Не запуская код, определите, что выведется в консоль:
+a. console.log(Number(true));
+4. Не запуская код, определите, что выведется в консоль:
+a. console.log(true + 1);
+5. Не запуская код, определите, что выведется в консоль:
+a. console.log(true + true)
+6. Не запуская код, определите, что выведется в консоль:
+a. console.log(true - true)
+7. Не запуская код, определите, что выведется в консоль:
+a. console.log( String(true) + Number(true) ) */
+
+
+
+// 1
+console.log(String(true));
+
+/* // 2
+console.log('a' + true); */
+
+/* // 3
+console.log(Number(true)); */
+
+/* // 4
+console.log(true + 1); */
+
+/* // 5
+console.log(true + true) */
+
+/* // 6
+console.log(true - true) */
+
+/* // 7
+console.log( String(true) + Number(true) ) */
