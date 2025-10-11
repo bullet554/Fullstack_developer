571ad097b60d83aab733c24ecc1d58a453ee4e0c
Ilya Karpov <ilya.karpov.93@mail.ru>
2025-10-12 00:24:55 +0400

    completed HW 2

---
+++ 2._JavaScript/Practical/Homework/HW_02/Task_002/script.js
@@ -0,0 +1,15 @@
+/* Задание 2
+Описание задачи: Перепишите следующий код с использованием тернарного
+оператора:
+
+let test = true;
+if (test === true) {
+console.log('+++');
+} else {
+console.log('---');
+}
+*/
+
+let test = false;
+
+console.log(test ? '+++' : '---');
