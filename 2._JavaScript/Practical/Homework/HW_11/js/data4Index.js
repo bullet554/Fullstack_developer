cc775aee41899cfcc1a61ffdc5803883ac60848e
Ilya Karpov <ilya.karpov.93@mail.ru>
2025-10-20 13:10:41 +0400

    completed HW 11

---
+++ 2._JavaScript/Practical/Homework/HW_11/js/data4Index.js
@@ -0,0 +1,44 @@
+const dataJSON = `[
+    {
+        "id": 1,
+        "img": "../img/product-1.jpg",
+        "name": "ELLERY X M'O CAPSULE",
+        "info": "Known for her sculptural takes on traditional tailoring, Australianarbiter of cool Kym Ellery teams up with Moda Operandi.",
+        "price": "52.00"
+    },
+    {
+        "id": 2,
+        "img": "../img/product-2.jpg",
+        "name": "ELLERY X M'O CAPSULE",
+        "info": "Known for her sculptural takes on traditional tailoring, Australianarbiter of cool Kym Ellery teams up with Moda Operandi.",
+        "price": "52.00"
+    },
+    {
+        "id": 3,
+        "img": "../img/product-3.jpg",
+        "name": "ELLERY X M'O CAPSULE",
+        "info": "Known for her sculptural takes on traditional tailoring, Australianarbiter of cool Kym Ellery teams up with Moda Operandi.",
+        "price": "52.00"
+    },
+    {
+        "id": 4,
+        "img": "../img/product-4.jpg",
+        "name": "ELLERY X M'O CAPSULE",
+        "info": "Known for her sculptural takes on traditional tailoring, Australianarbiter of cool Kym Ellery teams up with Moda Operandi.",
+        "price": "52.00"
+    },
+    {
+        "id": 5,
+        "img": "../img/product-5.jpg",
+        "name": "ELLERY X M'O CAPSULE",
+        "info": "Known for her sculptural takes on traditional tailoring, Australianarbiter of cool Kym Ellery teams up with Moda Operandi.",
+        "price": "52.00"
+    },
+    {
+        "id": 6,
+        "img": "../img/product-6.jpg",
+        "name": "ELLERY X M'O CAPSULE",
+        "info": "Known for her sculptural takes on traditional tailoring, Australianarbiter of cool Kym Ellery teams up with Moda Operandi.",
+        "price": "52.00"
+    }
+]`
---
+++ 2._JavaScript/Practical/Homework/HW_11/js/script.js
@@ -0,0 +1,47 @@
+const productsData = JSON.parse(dataJSON);
+
+const productBox = document.querySelector('.product__items');
+
+productsData.forEach(productData => {
+
+    const productEl = document.createElement('div');
+    productEl.classList.add('product__item');
+
+    const productImgWrapper = document.createElement('div');
+    productImgWrapper.classList.add('product__image-wrapper');
+
+    const productImg = document.createElement('img');
+    productImg.classList.add('product__img');
+    productImg.setAttribute('alt', 'Product image');
+    productImg.src = productData.img;
+
+    const productBtnAdd = document.createElement('button');
+    productBtnAdd.classList.add('product__add');
+    productBtnAdd.textContent = 'Add to Cart';
+
+    const productContent = document.createElement('div');
+    productContent.classList.add('product__content');
+
+    const productName = document.createElement('a');
+    productName.classList.add('product__name');
+    productName.textContent = productData.name;
+    productName.href = 'product.html';
+
+    const productInfo = document.createElement('p');
+    productInfo.classList.add('product__info');
+    productInfo.textContent = productData.info;
+
+    const productPrice = document.createElement('p');
+    productPrice.classList.add('product__price');
+    productPrice.textContent = productData.price;
+
+    productBox.appendChild(productEl);
+    productEl.appendChild(productImgWrapper);
+    productImgWrapper.appendChild(productImg);
+    productImgWrapper.appendChild(productBtnAdd);
+    productEl.appendChild(productContent);
+    productContent.appendChild(productName);
+    productContent.appendChild(productInfo);
+    productContent.appendChild(productPrice);
+
+});
