3aa6f13286047636171a5bc9c2930cf000be90c7
Ilya Karpov <ilya.karpov.93@mail.ru>
2025-10-20 17:34:21 +0400

    completed seminar 12

---
+++ 2._JavaScript/Practical/Seminars/Seminar_012/Task_001/js/data.js
@@ -0,0 +1,36 @@
+const dataProducts = `
+    [
+        {
+            "id": 1,
+            "img": "./img/product-1.jpg",
+            "name": "MANGO  PEOPLE  T-SHIRT",
+            "price": "300",
+            "color": "Red",
+            "size": "Xl"
+        },
+        {
+            "id": 2,
+            "img": "./img/product-2.jpg",
+            "name": "ELLERY X M'O CAPSULE",
+            "price": "52",
+            "color": "Black",
+            "size": "Xl"
+        },
+        {
+            "id": 3,
+            "img": "./img/product-1.jpg",
+            "name": "MANGO  T-SHIRT",
+            "price": "3000",
+            "color": "Blue",
+            "size": "Xl"
+        },
+        {
+            "id": 4,
+            "img": "./img/product-2.jpg",
+            "name": "ELLERY CAPSULE",
+            "price": "52000",
+            "color": "Brown",
+            "size": "L"
+        }
+    ]
+`
---
+++ 2._JavaScript/Practical/Seminars/Seminar_012/Task_001/js/script.js
@@ -0,0 +1,88 @@
+const productsData = JSON.parse(dataProducts);
+const productsBox = document.querySelector('.cart');
+
+productsData.forEach(productData => {
+    const productEl = document.createElement('div');
+    productEl.classList.add('cart-product');
+
+    // Изображение
+    const productImg = document.createElement('img');
+    productImg.classList.add('cart-product__img');
+    productImg.setAttribute('alt', 'Product image');
+    productImg.src = productData.img;
+
+    // Информация о товаре
+    const productInfo = document.createElement('div');
+    productInfo.classList.add('cart-product__info');
+
+    const productName = document.createElement('a');
+    productName.classList.add('cart-product__name');
+    productName.href = '#';
+    productName.textContent = productData.name;
+
+    const productPrice = document.createElement('p');
+    productPrice.classList.add('cart-product__text');
+    productPrice.textContent = 'Price: ';
+
+    const spanPrice = document.createElement('span');
+    spanPrice.classList.add('cart-product__text_select-color');
+    spanPrice.textContent = `$${productData.price}`;
+
+    const productColor = document.createElement('p');
+    productColor.classList.add('cart-product__text');
+    productColor.textContent = 'Color: ';
+
+    const spanColor = document.createElement('span');
+    spanColor.classList.add('cart-product__text_select-text');
+    spanColor.textContent = productData.color;
+
+    const productSize = document.createElement('p');
+    productSize.classList.add('cart-product__text');
+    productSize.textContent = 'Size: ';
+
+    const spanSize = document.createElement('span');
+    spanSize.classList.add('cart-product__text_select-text');
+    spanSize.textContent = productData.size;
+
+    const productQuantity = document.createElement('p');
+    productQuantity.classList.add('cart-product__text');
+    productQuantity.textContent = 'Quantity: ';
+
+    const inputQuantity = document.createElement('input');
+    inputQuantity.classList.add('cart-product__text_count');
+    inputQuantity.type = 'number';
+    inputQuantity.value = '2';
+
+    // Кнопка закрытия с SVG
+    const btnClose = document.createElement('button');
+    btnClose.classList.add('cart-product__close');
+
+    const svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+    svg.setAttribute('width', '18');
+    svg.setAttribute('height', '18');
+    svg.setAttribute('viewBox', '0 0 18 18');
+    svg.setAttribute('fill', 'none');
+
+    const path = document.createElementNS('http://www.w3.org/2000/svg', 'path');
+    path.setAttribute('d', 'M11.2453 9L17.5302 2.71516C17.8285 2.41741 17.9962 2.01336 17.9966 1.59191C17.997 1.17045 17.8299 0.76611 17.5322 0.467833C17.2344 0.169555 16.8304 0.00177586 16.4089 0.00140366C15.9875 0.00103146 15.5831 0.168097 15.2848 0.465848L9 6.75069L2.71516 0.465848C2.41688 0.167571 2.01233 0 1.5905 0C1.16868 0 0.764125 0.167571 0.465848 0.465848C0.167571 0.764125 0 1.16868 0 1.5905C0 2.01233 0.167571 2.41688 0.465848 2.71516L6.75069 9L0.465848 15.2848C0.167571 15.5831 0 15.9877 0 16.4095C0 16.8313 0.167571 17.2359 0.465848 17.5342C0.764125 17.8324 1.16868 18 1.5905 18C2.01233 18 2.41688 17.8324 2.71516 17.5342L9 11.2493L15.2848 17.5342C15.5831 17.8324 15.9877 18 16.4095 18C16.8313 18 17.2359 17.8324 17.5342 17.5342C17.8324 17.2359 18 16.8313 18 16.4095C18 15.9877 17.8324 15.5831 17.5342 15.2848L11.2453 9Z');
+    path.setAttribute('fill', '#575757');
+
+    // Обработчик удаления
+    btnClose.addEventListener('click', () => { productEl.remove(); }); // Удаляем родительский элемент .cart-product
+
+    productsBox.appendChild(productEl);
+    productEl.appendChild(productImg);
+    productEl.appendChild(productInfo);
+    productInfo.appendChild(productName);
+    productInfo.appendChild(productPrice);
+    productPrice.appendChild(spanPrice);
+    productInfo.appendChild(productColor);
+    productColor.appendChild(spanColor);
+    productInfo.appendChild(productSize);
+    productSize.appendChild(spanSize);
+    productInfo.appendChild(productQuantity);
+    productQuantity.appendChild(inputQuantity);
+    svg.appendChild(path);
+    btnClose.appendChild(svg);
+    productEl.appendChild(btnClose);
+});
