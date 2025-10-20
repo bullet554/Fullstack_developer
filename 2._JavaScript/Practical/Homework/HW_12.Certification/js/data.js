f6fc524fd08b607a4f0148323b9e01c9c52a8f99
Ilya Karpov <ilya.karpov.93@mail.ru>
2025-10-20 19:46:49 +0400

    1. Rename html file. 2. Started HW 12.

---
+++ 2._JavaScript/Practical/Homework/HW_12.Certification/js/data.js
@@ -0,0 +1,81 @@
+const dataIndexJSON = `[
+    {
+        "id": 1,
+        "img": "../img/product-1.jpg",
+        "name": "ELLERY X M'O CAPSULE",
+        "info": "Known for her sculptural takes on traditional tailoring, Australianarbiter of cool Kym Ellery teams up with Moda Operandi.",
+        "price": "52.00"
+    },
+    {
+        "id": 2,
+        "img": "../img/product-2.jpg",
+        "name": "ELLERY X M'O CAPSULE",
+        "info": "Known for her sculptural takes on traditional tailoring, Australianarbiter of cool Kym Ellery teams up with Moda Operandi.",
+        "price": "52.00"
+    },
+    {
+        "id": 3,
+        "img": "../img/product-3.jpg",
+        "name": "ELLERY X M'O CAPSULE",
+        "info": "Known for her sculptural takes on traditional tailoring, Australianarbiter of cool Kym Ellery teams up with Moda Operandi.",
+        "price": "52.00"
+    },
+    {
+        "id": 4,
+        "img": "../img/product-4.jpg",
+        "name": "ELLERY X M'O CAPSULE",
+        "info": "Known for her sculptural takes on traditional tailoring, Australianarbiter of cool Kym Ellery teams up with Moda Operandi.",
+        "price": "52.00"
+    },
+    {
+        "id": 5,
+        "img": "../img/product-5.jpg",
+        "name": "ELLERY X M'O CAPSULE",
+        "info": "Known for her sculptural takes on traditional tailoring, Australianarbiter of cool Kym Ellery teams up with Moda Operandi.",
+        "price": "52.00"
+    },
+    {
+        "id": 6,
+        "img": "../img/product-6.jpg",
+        "name": "ELLERY X M'O CAPSULE",
+        "info": "Known for her sculptural takes on traditional tailoring, Australianarbiter of cool Kym Ellery teams up with Moda Operandi.",
+        "price": "52.00"
+    }
+]`;
+
+const dataCartJSON = `
+    [
+        {
+            "id": 1,
+            "img": "../img/cart/product-1.jpg",
+            "name": "MANGO  PEOPLE  T-SHIRT",
+            "price": "300",
+            "color": "Red",
+            "size": "Xl"
+        },
+        {
+            "id": 2,
+            "img": "../img/cart/product-2.jpg",
+            "name": "ELLERY X M'O CAPSULE",
+            "price": "52",
+            "color": "Black",
+            "size": "Xl"
+        },
+        {
+            "id": 3,
+            "img": "../img/cart/product-1.jpg",
+            "name": "MANGO  T-SHIRT",
+            "price": "3000",
+            "color": "Blue",
+            "size": "Xl"
+        },
+        {
+            "id": 4,
+            "img": "../img/cart/product-2.jpg",
+            "name": "ELLERY CAPSULE",
+            "price": "52000",
+            "color": "Brown",
+            "size": "L"
+        }
+    ]
+`;
